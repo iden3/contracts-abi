3d307cd835cba2d14f53e67bdd6ed39a9f5c1440
daveroga <daveroga@gmail.com>
2025-07-29 10:14:12 +0200

    add errors modules

---
+++ state/go/abi/error.go
@@ -0,0 +1,42 @@
+package abi
+
+import (
+	"strings"
+
+	"errors"
+)
+
+var (
+	msgErrIdentityDoesNotExist            = "Identity does not exist"
+	msgErrIdentityDoesNotExistCustomError = "IdentityDoesNotExist"
+	msgErrRootDoesNotExist                = "Root does not exist"
+	msgErrRootDoesNotExistCustomError     = "RootDoesNotExist"
+	msgErrStateDoesNotExist               = "State does not exist"
+	msgErrStateDoesNotExistCustomError    = "StateDoesNotExist"
+)
+
+var (
+	ErrIdentityDoesNotExist = errors.New(msgErrIdentityDoesNotExist)
+	ErrRootDoesNotExist     = errors.New(msgErrRootDoesNotExist)
+	ErrStateDoesNotExist    = errors.New(msgErrStateDoesNotExist)
+)
+
+func isError(err error, errorMessage string) bool {
+	if err == nil {
+		return false
+	}
+	return strings.Contains(err.Error(), errorMessage)
+
+}
+
+func IsErrIdentityDoesNotExist(err error) bool {
+	return isError(err, msgErrIdentityDoesNotExist) || isError(err, msgErrIdentityDoesNotExistCustomError)
+}
+
+func IsErrRootDoesNotExist(err error) bool {
+	return isError(err, msgErrRootDoesNotExist) || isError(err, msgErrRootDoesNotExistCustomError)
+}
+
+func IsErrStateDoesNotExist(err error) bool {
+	return isError(err, msgErrStateDoesNotExist) || isError(err, msgErrStateDoesNotExistCustomError)
+}
